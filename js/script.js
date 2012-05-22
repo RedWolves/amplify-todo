2821cc272ffdc8bf69cdd37dd728a5a291923d60
Ralph Whitbeck <ralph.whitbeck@gmail.com>
2012-05-22 00:01:24 -0400

    using on instead of bind, old habits heh.

--- js/script.js
+++ js/script.js
@@ -2,14 +2,14 @@ $(function() {
 
   amplify.publish( "initTasks" );
 
-  $( "#tasks" ).prev( "button" ).bind("click", function() {
+  $( "#tasks" ).prev( "button" ).on("click", function() {
     amplify.publish( "newTask" );
   }).end()
-               .find("input[type=checkbox]").bind("click", function() {
+               .find("input[type=checkbox]").on("click", function() {
                   amplify.publish( "closeTask", $(this) );
                });
 
-  $( "#new" ).find( "button" ).bind( "click", function() {
+  $( "#new" ).find( "button" ).on( "click", function() {
     amplify.publish( "addTask");
   } );
 
