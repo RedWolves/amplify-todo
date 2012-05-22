0986575ff7b04d9a3bcc832a5eab7bcd65752260
Ralph Whitbeck <ralph.whitbeck@gmail.com>
2012-05-22 00:06:10 -0400

    code formatting

--- js/script.js
+++ js/script.js
@@ -3,16 +3,15 @@ $(function() {
   amplify.publish( "initTasks" );
 
   $( "#tasks" ).prev( "button" ).on("click", function() {
-    amplify.publish( "newTask" );
-  }).end()
+                  amplify.publish( "newTask" );
+                }).end()
                .find("input[type=checkbox]").on("click", function() {
                   amplify.publish( "closeTask", $(this) );
-               });
+                });
 
   $( "#new" ).find( "button" ).on( "click", function() {
-    amplify.publish( "addTask");
-  } );
-
+                amplify.publish( "addTask");
+              } );
 });
 
 
